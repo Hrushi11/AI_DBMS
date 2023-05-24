1f1b918f53238ea0a86c50a11b4f9a86fd05e797
Hrushikesh Kachgunde <73577192+Hrushi11@users.noreply.github.com>
2023-05-24 22:40:14 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,42 +1,95 @@
-# def A_star(strt_node, goal_node):
-#     v = [strt_node]
-#     q = [strt_node]
-#     for vertex in graph[strt_node]:
-#         f_n = vertex[1] + h(vertex[0])
-#         # print(f'{vertex} -- {f_n}')
+from collections import namedtuple
 
-#     while len(q):
-#         vertex = q.pop(0)
-#         for neighs in graph[vertex]:
-#             if neighs[0] not in v:
-#                 print(neighs)
-#                 q.append(neighs[0])
-#                 v.append(neighs[0])
+# Define a named tuple to represent a state in the game
+State = namedtuple('State', ['x', 'y'])
 
+# Define the grid size
+GRID_SIZE = 5
 
+# Define the start and goal coordinates
+start_state = State(0, 0)
+goal_state = State(4, 4)
 
-# # Input data
-# def h(n):
-#     H_dist = {
-#         'S': 5,
-#         'A': 3,
-#         'B': 4,
-#         'C': 2,
-#         'D': 6,
-#         'G': 0,
-#     }
+# Define the grid with blocked cells represented by 'X'
+grid = [
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', 'X', ' ', 'X', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', 'X', ' ', 'X', ' '],
+    [' ', ' ', ' ', ' ', ' ']
+]
 
-#     return H_dist[n]
+def generate_neighbors(state):
+    neighbors = []
 
-# graph = {
-#     'S': [('A', 1), ('G', 10)],
-#     'A': [('B', 2), ('C', 1)],
-#     'B': [('D', 2)],
-#     'C': [('D', 3), ('G', 4)],
-#     'D': [('G', 2)],
-#     'G': [],
-# }
+    # Define the possible movement directions: up, down, left, right
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-# A_star('S', 'G')
+    for direction in directions:
+        new_x = state.x + direction[0]
+        new_y = state.y + direction[1]
 
-print('Coming SOON...')
+        # Check if the new coordinates are within the grid bounds
+        if 0 <= new_x < GRID_SIZE and 0 <= new_y < GRID_SIZE:
+            # Check if the new cell is not blocked
+            if grid[new_x][new_y] != 'X':
+                neighbors.append(State(new_x, new_y))
+
+    return neighbors
+
+def cost(state, neighbor):
+    # Assume a cost of 1 to move from any state to its neighbor
+    return 1
+
+def heuristic(state):
+    # Use the Manhattan distance as the heuristic function
+    return abs(state.x - goal_state.x) + abs(state.y - goal_state.y)
+
+def astar(start_state, goal_state, heuristic):
+    open_set = {start_state}
+    closed_set = set()
+
+    g_score = {start_state: 0}
+    f_score = {start_state: heuristic(start_state)}
+
+    while open_set:
+        current_state = min(open_set, key=lambda state: f_score[state])
+
+        if current_state == goal_state:
+            return reconstruct_path(current_state)
+
+        open_set.remove(current_state)
+        closed_set.add(current_state)
+
+        neighbors = generate_neighbors(current_state)
+
+        for neighbor in neighbors:
+            g = g_score[current_state] + cost(current_state, neighbor)
+
+            if neighbor in closed_set and g >= g_score.get(neighbor, float('inf')):
+                continue
+
+            if neighbor not in open_set or g < g_score.get(neighbor, float('inf')):
+                g_score[neighbor] = g
+                f_score[neighbor] = g + heuristic(neighbor)
+                neighbor.parent = current_state
+                open_set.add(neighbor)
+
+    return None
+
+def reconstruct_path(state):
+    path = [state]
+    while hasattr(state, 'parent') and state.parent is not None:
+        state = state.parent
+        path.append(state)
+    return list(reversed(path))
+
+# Call the A* algorithm with the given parameters
+path = astar(start_state, goal_state, heuristic)
+
+if path is not None:
+    print("Path found:")
+    for state in path:
+        print(f"({state.x}, {state.y})")
+else:
+    print("No path found.")
